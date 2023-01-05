5796ce283a311c56794c8b1ac89667c563b6bfe1
Themuuln <zerone.offical@gmail.com>
2023-01-06 01:44:07 +0800

    🧪 --RGB7209

---
+++ RGB7209.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, n, m = 1;
+  cin >> a >> n;
+  for (int i = 0; i < n; i++)
+  {
+    m *= a;
+  }
+  cout << m << endl;
+}
+
+// Өгөгдсөн тооны n зэргийг ол.
+// Input
+
+// Нэг мөрөнд Int төрлийн эерэг 2  тоо зайгаар тусгаарлагдан өгөгдөнө.
+// Output
+
+// an
